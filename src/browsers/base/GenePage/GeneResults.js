1052d6710f65cf5ce297180d86e2a6a4431869df
Nick Watts <nwatts@broadinstitute.org>
2020-11-25 17:21:00 -0500

    Flag genes based on results in other studies

--- src/browsers/base/GenePage/GeneResults.js
+++ src/browsers/base/GenePage/GeneResults.js
@@ -1,20 +1,72 @@
 import PropTypes from 'prop-types'
 import React from 'react'
 
+import { Badge, ExternalLink, List, ListItem } from '@gnomad/ui'
+
 import datasetConfig from '../../datasetConfig'
 import geneResultComponents from '../../geneResultComponents'
 
 const DatasetGeneResultComponent = geneResultComponents[datasetConfig.datasetId]
 
+const studyPhenotypes = {
+  ASC: 'Autism',
+  BipEx: 'Bipolar disorder',
+  Epi25: 'Epilepsy',
+  SCHEMA: 'Schizophrenia',
+}
+
 const GeneResults = DatasetGeneResultComponent
-  ? ({ results }) => {
-      const { [datasetConfig.datasetId]: datasetResults } = results
-      return <DatasetGeneResultComponent results={datasetResults || {}} />
+  ? ({ gene, results }) => {
+      const { [datasetConfig.datasetId]: datasetResults, ...otherResults } = results
+
+      const associatedOtherResults = Object.keys(otherResults)
+        .map((dataset) => ({
+          dataset,
+          r: otherResults[dataset],
+        }))
+        .filter(({ dataset, r }) => {
+          if (dataset === 'SCHEMA' && r.meta['P meta'] <= 7.9e-5) {
+            return true
+          }
+          if (dataset === 'ASC' && r.All.qval <= 0.1) {
+            return true
+          }
+          return false
+        })
+
+      return (
+        <>
+          <DatasetGeneResultComponent results={datasetResults || {}} />
+          {associatedOtherResults.length > 0 && (
+            <>
+              <p>
+                <Badge level="info">Note</Badge> Other studies have found this gene to be associated
+                with:
+              </p>
+              <List>
+                {associatedOtherResults.map(({ dataset }) => (
+                  <ListItem key={dataset}>
+                    {studyPhenotypes[dataset]} -{' '}
+                    <ExternalLink
+                      href={`https://${dataset.toLowerCase()}.broadinstitute.org/gene/${
+                        gene.gene_id
+                      }`}
+                    >
+                      see details in {dataset} browser
+                    </ExternalLink>
+                  </ListItem>
+                ))}
+              </List>
+            </>
+          )}
+        </>
+      )
     }
   : () => null
 
 GeneResults.propTypes = {
   gene: PropTypes.shape({
+    gene_id: PropTypes.string.isRequired,
     symbol: PropTypes.string,
   }).isRequired,
   results: PropTypes.objectOf(PropTypes.objectOf(PropTypes.any)).isRequired,
