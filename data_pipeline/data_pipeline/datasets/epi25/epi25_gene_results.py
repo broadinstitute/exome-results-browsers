b8b685a01a202b28f3eb204dba0d29d63fd3d2a0
Riley Grant <grantrileyh@gmail.com>
2023-01-23 09:56:11 -0600

    Update pipeline for new Epi25 data

--- data_pipeline/data_pipeline/datasets/epi25/epi25_gene_results.py
+++ data_pipeline/data_pipeline/datasets/epi25/epi25_gene_results.py
@@ -4,47 +4,72 @@ from data_pipeline.config import pipeline_config
 
 
 def prepare_gene_results():
-    ds = hl.import_table(
-        pipeline_config.get("Epi25", "gene_results_path"),
-        delimiter=",",
-        missing="NA",
-        quote='"',
-        types={
-            "gene_id": hl.tstr,
-            "gene_name": hl.tstr,
-            "description": hl.tstr,
-            "pval_meta": hl.tfloat,
-            "analysis_group": hl.tstr,
-            # LoF
-            "xcase_lof": hl.tint,
-            "xctrl_lof": hl.tint,
-            "pval_lof": hl.tfloat,
-            # MPC
-            "xcase_mpc": hl.tint,
-            "xctrl_mpc": hl.tint,
-            "pval_mpc": hl.tfloat,
-            # Inframe indel
-            "xcase_infrIndel": hl.tint,
-            "xctrl_infrIndel": hl.tint,
-            "pval_infrIndel": hl.tfloat,
-        },
+    results = hl.read_table(pipeline_config.get("Epi25", "gene_results_path"))
+
+    results = results.select_globals()
+
+    # Select result fields, discard gene information
+    results = results.select(
+        "gene_id",
+        "analysis_group",
+        "case_count",
+        "control_count",
+        "n_cases",
+        "n_controls",
+        "pval",
+        "OR",
     )
 
-    ds = ds.drop("gene_name", "description")
+    results = results.annotate(OR=hl.float(results.OR))
 
     # Rename EE group to DEE
-    ds = ds.annotate(analysis_group=hl.if_else(ds.analysis_group == "EE", "DEE", ds.analysis_group))
+    results = results.annotate(analysis_group=hl.if_else(results.analysis_group == "EE", "DEE", results.analysis_group))
 
-    # "Meta" p-val was carried over from SCHEMA's data format but isn't descriptive of Epi25
-    ds = ds.rename({"pval_meta": "pval"})
+    final_results = None
 
-    ds = ds.group_by("gene_id").aggregate(group_results=hl.agg.collect(ds.row_value))
-    ds = ds.annotate(
+    consequence_categories = results.aggregate(hl.agg.collect_as_set(results.consequence_category))
+    per_category_fields = [
+        "case_count",
+        "control_count",
+        "pval",
+        "OR",
+    ]
+    for category in consequence_categories:
+        category_results = results.filter(results.consequence_category == category)
+        category_results = category_results.key_by("gene_id", "analysis_group")
+        category_results = category_results.select(
+            n_cases=category_results.n_cases,
+            n_controls=category_results.n_controls,
+            **{f"{category}_{field}": category_results[field] for field in per_category_fields},
+        )
+
+        if final_results:
+            final_results = final_results.join(category_results.drop("n_cases", "n_controls"), "outer")
+
+            # N cases/controls should be the same for all consequence categories for a gene/analysis group.
+            # However, if there are no variants of a certain consequence category found in a gene, then
+            # N cases/controls for that gene/analysis group/consequence category will be missing.
+            final_results = final_results.annotate(
+                n_cases=hl.or_else(
+                    final_results.n_cases, category_results[final_results.gene_id, final_results.analysis_group].n_cases
+                ),
+                n_controls=hl.or_else(
+                    final_results.n_controls,
+                    category_results[final_results.gene_id, final_results.analysis_group].n_controls,
+                ),
+            )
+        else:
+            final_results = category_results
+
+    final_results = final_results.group_by("gene_id").aggregate(
+        group_results=hl.agg.collect(final_results.row.drop("gene_id"))
+    )
+    final_results = final_results.annotate(
         group_results=hl.dict(
-            ds.group_results.map(
-                lambda group_result: (group_result.analysis_group, group_result.drop("gene_id", "analysis_group"))
+            final_results.group_results.map(
+                lambda group_result: (group_result.analysis_group, group_result.drop("analysis_group"))
             )
         )
     )
 
-    return ds
+    return final_results
