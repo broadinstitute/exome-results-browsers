a8eded3a86985140c5534c65413c8dfa6e5608da
Riley Grant <grantrileyh@gmail.com>
2025-02-05 16:40:51 -0600

    feat(data-pipelines): run VEP on IBD variants and annotate with transcript csqs

--- data_pipeline/data_pipeline/datasets/ibd/ibd_variant_results.py
+++ data_pipeline/data_pipeline/datasets/ibd/ibd_variant_results.py
@@ -1,9 +1,13 @@
+import os
+
 import hail as hl
 
 from data_pipeline.config import pipeline_config
 
 
 def prepare_variant_results():
+    staging_output_path = pipeline_config.get("output", "staging_path")
+
     results = hl.read_table(pipeline_config.get("IBD", "variant_results_path"))
 
     # Get unique variants from results table
@@ -48,6 +52,43 @@ def prepare_variant_results():
     # Merge variant annotations for canonical transcripts
     annotations = hl.read_table(pipeline_config.get("IBD", "variant_annotations_path"))
 
+
+    # add vep to annotations to store in info field
+    vepped_path = os.path.join(staging_output_path, 'ibd', 'variants_vepped.ht')
+    if not hl.hadoop_exists(vepped_path):
+        print("no vepped table found")
+        variants_to_vep = variants.select()
+        vepped_variants = hl.vep(variants_to_vep)
+        vepped_variants.write(vepped_path, overwrite=True)
+
+    vepped_variants_ht = hl.read_table(vepped_path)
+
+    # annotations = annotations.annotate(vep=hl.json(vepped_variants_ht[annotations.locus, annotations.alleles].vep))
+    annotations = annotations.annotate(vep=vepped_variants_ht[annotations.locus, annotations.alleles].vep)
+
+    annotations = annotations.annotate(
+        transcript_consequences=annotations.vep.transcript_consequences.map(
+            lambda tc: hl.struct(
+                consequence_terms = tc.consequence_terms,
+                domains = tc.domains,
+                gene_id = tc.gene_id,
+                gene_symbol = tc.gene_symbol,
+                hgnc_id = tc.hgnc_id,
+                hgvsc = tc.hgvsc,
+                hgvsp = tc.hgvsp,
+                canonical = tc.canonical,
+                lof = tc.lof,
+                lof_flags = tc.lof_flags,
+                lof_filter = tc.lof_filter,
+                polyphen_prediction = tc.polyphen_prediction,
+                sift_prediction = tc.sift_prediction,
+                transcript_id = tc.transcript_id
+            )
+        )
+    )
+
+    annotations = annotations.annotate(transcript_consequences=hl.json(annotations.transcript_consequences))
+
     # Not actually sure which annotations we need
     annotations = annotations.select(
         gene_id=annotations.gene_id_canonical,
@@ -61,10 +102,24 @@ def prepare_variant_results():
             splice_ai=annotations.splice_ai_score,
             # TODO: ask about which annotations we want to display?
             sift=annotations.sift_score_canonical,
+            transcript_consequences=annotations.transcript_consequences,
+            # vep=annotations.vep,
             # primate_ai=annotations.primate_ai_score,
         ),
     )
 
     variants = variants.annotate(**annotations[variants.locus, variants.alleles])
+
+
+    vepped_path = os.path.join(staging_output_path, 'ibd', 'variants_vepped.ht')
+    if not hl.hadoop_exists(vepped_path):
+        print("no vepped table found")
+        exit(1)
+        variants_to_vep = variants.select()
+        vepped_variants = hl.vep(variants_to_vep)
+        vepped_variants.write(vepped_path, overwrite=True)
+    vepped_variants_ht = hl.read_table(vepped_path)
+
+    variants = variants.annotate(vep=vepped_variants_ht[variants.locus, variants.alleles].vep)
 
     return variants
