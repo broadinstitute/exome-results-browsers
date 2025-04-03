9110371b7c0985d1c3c745e4b588b3b2f2d996c5
Riley Grant <grantrileyh@gmail.com>
2025-04-03 11:57:30 -0500

    feat(data-pipelines): allow dataproc cluster to take add'l arguments

--- data_pipeline/start_dataproc_cluster.py
+++ data_pipeline/start_dataproc_cluster.py
@@ -8,7 +8,11 @@ import subprocess
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("--dry-run", action="store_true", help="Print cluster creation command without running it")
+    parser.add_argument("cluster_args", nargs=argparse.REMAINDER)
     args = parser.parse_args()
+
+    if args.cluster_args and args.cluster_args[0] == "--":
+        args.cluster_args = args.cluster_args[1:]
 
     # Set working directory so that config.py finds pipeline_config.ini
     os.chdir(os.path.dirname(os.path.abspath(__file__)))
@@ -21,7 +25,7 @@ def main():
         "start",
         "exome-results",
         "--max-idle=1h",
-    ]
+    ] + args.cluster_args
 
     for option in ["project", "region", "zone", "service-account"]:
         value = pipeline_config.get("dataproc", option, fallback=None)
