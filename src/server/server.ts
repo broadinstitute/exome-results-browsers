6f10b2782fa5f1037b6f95b72433be6e76638431
Riley Grant <grantrileyh@gmail.com>
2025-05-20 16:58:45 -0500

    feat(backend): add add'l subdomain to dataset mapping
    ibd breaks the pattern of subdomain == datasetId, here we add an easy extensible way to manually add other mappings, e.g. subdomain: 'ibdseq' -> dataset: 'ibd'

--- src/server/server.ts
+++ src/server/server.ts
@@ -173,13 +173,18 @@ if (isDevelopment) {
   )
   getDatasetForRequest = () => devDataset
 } else {
+
+  const subdomainOverrides: Record<string, string> = {
+    ibdseq: 'ibd',
+  };
+
   const datasetBySubdomain: Record<string, string> = Object.keys(metadata.datasets).reduce(
     (acc, dataset) => ({
       ...acc,
       [dataset.toLowerCase()]: dataset,
     }),
-    {}
-  )
+    { ...subdomainOverrides }
+  );
   getDatasetForRequest = (req: express.Request) => datasetBySubdomain[req.subdomains[0]]
 }
 
@@ -191,7 +196,7 @@ const PASSWORD_PROTECTED_DATASETS = ['BipEx2', 'SCHEMA2']
 
 const CORRECT_PASSWORD = process.env.DEMO_PASSWORD
   ? // Remove the ""s from development env var with a regex
-    process.env.DEMO_PASSWORD.replace(/^"|"$/g, '') || 'password'
+  process.env.DEMO_PASSWORD.replace(/^"|"$/g, '') || 'password'
   : 'password'
 
 const activeTokens = new Set()
@@ -202,7 +207,7 @@ app.post('/api/auth', (req: Request, res: Response) => {
   let dataset: any
   try {
     dataset = getDatasetForRequest(req)
-  } catch (err) {} // eslint-disable-line no-empty
+  } catch (err) { } // eslint-disable-line no-empty
 
   if (!dataset) {
     res.status(500).json({ message: 'Unknown dataset' })
@@ -240,7 +245,7 @@ app.post('/api/check-auth', (req: Request, res: Response) => {
   let dataset: any
   try {
     dataset = getDatasetForRequest(req)
-  } catch (err) {} // eslint-disable-line no-empty
+  } catch (err) { } // eslint-disable-line no-empty
 
   if (!dataset) {
     res.status(500).json({ message: 'Unknown dataset' })
@@ -266,7 +271,7 @@ app.use('/', (req: Request, res: Response, next: NextFunction) => {
   let dataset: any
   try {
     dataset = getDatasetForRequest(req)
-  } catch (err) {} // eslint-disable-line no-empty
+  } catch (err) { } // eslint-disable-line no-empty
 
   if (!dataset) {
     res.status(500).json({ message: 'Unknown dataset' })
