de77bc956f0c3c2814c46216d0b69f80ff0d0514
Riley Grant <grantrileyh@gmail.com>
2025-09-11 14:59:35 -0500

    feat(pipelines): gp2 combine re-orders wgs annotations

--- data_pipeline/data_pipeline/datasets/gp2/gp2_combine_input_datasets.py
+++ data_pipeline/data_pipeline/datasets/gp2/gp2_combine_input_datasets.py
@@ -1,11 +1,33 @@
 def combine_variant_results(ces_variants_ht, wgs_variants_ht):
     print("Combining variant results")
+
+    print("TEMP - SUBSETTING TO Chr22")
+    ces_variants_ht = ces_variants_ht.filter(ces_variants_ht.locus.contig == "chr22")
+    wgs_variants_ht = wgs_variants_ht.filter(wgs_variants_ht.locus.contig == "chr22")
+
     combined_variants_ht = ces_variants_ht.union(wgs_variants_ht, unify=True)
     return combined_variants_ht
 
 
 def combine_variant_annotations(ces_annotations_ht, wgs_annotations_ht):
     print("Combining variant annotations")
+
+    print("TEMP - SUBSETTING TO Chr22")
+    ces_annotations_ht = ces_annotations_ht.filter(ces_annotations_ht.locus.contig == "chr22")
+    wgs_annotations_ht = wgs_annotations_ht.filter(wgs_annotations_ht.locus.contig == "chr22")
+
+    # wgs is ordered differently, re-order to allow union
+    wgs_annotations_ht = wgs_annotations_ht.select(
+        wgs_annotations_ht.variant_id,
+        wgs_annotations_ht.gene_id,
+        wgs_annotations_ht.transcript_id,
+        wgs_annotations_ht.consequence,
+        wgs_annotations_ht.gene_name,
+        wgs_annotations_ht.hgvsc,
+        wgs_annotations_ht.hgvsp,
+        wgs_annotations_ht.cadd,
+    )
+
     combined_annotations_ht = ces_annotations_ht.union(wgs_annotations_ht)
     return combined_annotations_ht
 
