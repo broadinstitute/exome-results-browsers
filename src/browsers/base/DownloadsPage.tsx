29e01e9ff1effd9a663953d39ebbafb0af7da924
Riley Grant <grantrileyh@gmail.com>
2026-04-24 14:59:25 -0500

    feat(SCHEMA2): add SCHEMA1 data links to downloads page

--- src/browsers/base/DownloadsPage.tsx
+++ src/browsers/base/DownloadsPage.tsx
@@ -28,7 +28,7 @@ type DatasetDownloadLinksProps = {
   datasetId: DatasetId
 }
 
-const DatasetDownloadLinks = ({ datasetId }: DatasetDownloadLinksProps) => {
+const DatasetDownloadLinkList = ({ datasetId }: DatasetDownloadLinksProps) => {
   return (
     <List>
       <ListItem>
@@ -50,11 +50,26 @@ const DatasetDownloadLinks = ({ datasetId }: DatasetDownloadLinksProps) => {
   )
 }
 
+const DatasetDownloads = ({ datasetId }: DatasetDownloadLinksProps) => {
+  return (
+    <>
+      <DatasetDownloadLinkList datasetId={datasetId} />
+      {datasetId === "SCHEMA2" &&
+        <>
+          <h3 style={{ marginBottom: "0.5rem" }}>Previous SCHEMA release data downloads</h3>
+          <p style={{ marginTop: "0rem" }}>The prior SCHEMA analysis and dataset was released September 10th, 2020.</p>
+          <DatasetDownloadLinkList datasetId={"SCHEMA"} />
+        </>
+      }
+    </>
+  )
+}
+
 export default () => {
   const datasetsWithoutDownloads: DatasetId[] = ['GP2', 'IBD']
   return (
     <InfoPage title="Downloads">
-      <DatasetDownloadLinks datasetId={datasetConfig.datasetId} />
+      <DatasetDownloads datasetId={datasetConfig.datasetId} />
 
       <h2>Other Studies</h2>
       {otherDatasets
