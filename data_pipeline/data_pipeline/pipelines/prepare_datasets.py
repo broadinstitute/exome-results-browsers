83eede656984a0855279cf7cb982944d40824736
Riley Grant <grantrileyh@gmail.com>
2026-06-15 13:23:00 -0500

    feat(pipelines): process ClinVar dataset in pipeline helpers

--- data_pipeline/data_pipeline/pipelines/prepare_datasets.py
+++ data_pipeline/data_pipeline/pipelines/prepare_datasets.py
@@ -81,6 +81,19 @@ def prepare_dataset(dataset_id, test_genes, output_local):
         validate_gene_results_table(gene_results)
         gene_results.write(os.path.join(output_path, "gene_results.ht"), overwrite=True)
 
+    elif dataset_id.lower() == "ClinVarGRCh38".lower():
+        print(f"\n\n === Preparing {dataset_id} variants hail table")
+        clinvar_grch38_module = importlib.import_module("data_pipeline.datasets.clinvar.clinvar_grch38")
+
+        ht_clinvar_variants = clinvar_grch38_module.prepare_clinvar_variants(test_genes)
+        ht_clinvar_variants.write(os.path.join(output_path, "variant_results.ht"), overwrite=True)
+
+        clinvar_grch38_dummy_genes_module = importlib.import_module(
+            "data_pipeline.datasets.clinvar.clinvar_grch38_dummy_genes"
+        )
+        ht_clinvar_genes = clinvar_grch38_dummy_genes_module.prepare_gene_results(test_genes, output_root)
+        ht_clinvar_genes.write(os.path.join(output_path, "gene_results.ht"), overwrite=True)
+
     else:
         print(f"\n\n === Preparing {dataset_id} variants hail table")
         variant_results_module = importlib.import_module(
@@ -130,7 +143,7 @@ def main():
     else:
         datasets_to_prepare = all_datasets
 
-    print(f"\nPrepareing datasets: {list(datasets_to_prepare)} ...\n\n")
+    print(f"\nPreparing datasets: {list(datasets_to_prepare)} ...\n\n")
 
     # hl.init()
     hl.init(
