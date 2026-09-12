b290660d78ed9b81e60b2019399788eca5c81eef
Riley Grant <grantrileyh@gmail.com>
2026-09-12 09:32:08 -0500

    feat(frontend): add typing to SCHEMAs gene columns via accessors

--- src/browsers/schema/SCHEMABrowser.tsx
+++ src/browsers/schema/SCHEMABrowser.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 
 import ExomeResultsBrowser, {
+  GeneResultColumnConfig,
   RenderVariantAttributes,
   VariantColumnConfig,
   VariantCustomFilter,
@@ -14,6 +15,7 @@ import SCHEMAAboutPage from './SCHEMAAboutPage'
 import SCHEMAHomePage from './SCHEMAHomePage'
 import SCHEMATermsPage from './SCHEMATermsPage'
 import SCHEMAVariantFilter from './SCHEMAVariantFilter'
+import { SchemaGeneRow } from './schemaGeneTypes'
 import { SchemaVariantInfo, SchemaVariantRow } from './schemaVariantTypes'
 
 const variantConsequences = [...vepConsequences]
@@ -71,6 +73,178 @@ export const schemaAnalysisGroups = ['meta'] as const
 export type SCHEMAAnalysisGroup = typeof schemaAnalysisGroups[number]
 export const schemaDefaultAnalysisGroup: SCHEMAAnalysisGroup = 'meta'
 
+const schemaCaseControlPValueColumn: GeneResultColumnConfig<SchemaGeneRow, number> = {
+  key: 'schema_case_control_p_value',
+  heading: 'Case-Control SCHEMA P-value',
+  tooltip:
+    'SCHEMA p-value of the CMH p-value from PTV burden and the CMH p-value from PTV + missense burden.',
+  minWidth: 100,
+  accessor: (row) => row.schema_case_control_p_value,
+  render: (value) => renderStringOrFloatPvalueAsScientific({ value: value }),
+}
+
+const ptvCaseCarrierColumn: GeneResultColumnConfig<SchemaGeneRow, number> = {
+  key: 'ptv_case_carrier',
+  heading: 'Case PTV',
+  tooltip:
+    'Protein-truncating variants (PTVs) classified as high-confidence by LOFTEE: stop-gained, frameshift, and essential splice acceptor and donor sites. Aggregated counts from variants with minor allele count [MAC] <= 15',
+  minWidth: 70,
+  accessor: (row) => row.ptv_case_carrier,
+  render: (value) => renderCount(value),
+}
+
+const ptvControlCarrierColumn: GeneResultColumnConfig<SchemaGeneRow, number> = {
+  key: 'ptv_control_carrier',
+  heading: 'Control PTV',
+  tooltip:
+    'Protein-truncating variants (PTVs) classified as high-confidence by LOFTEE: stop-gained, frameshift, and essential splice acceptor and donor sites. Aggregated counts from variants with minor allele count [MAC] <= 15',
+  minWidth: 70,
+  accessor: (row) => row.ptv_control_carrier,
+  render: (value) => renderCount(value),
+}
+
+const ptvMisCaseCarrierColumn: GeneResultColumnConfig<SchemaGeneRow, number> = {
+  key: 'ptv_mis_case_carrier',
+  heading: 'Case PTV + Missense',
+  tooltip:
+    'Protein-truncating variants (PTVs) classified as high-confidence by LOFTEE: stop-gained, frameshift, and essential splice acceptor and donor sites, and missense variants predicted to be damaging (mean missense rank percentile >= 93%). Aggregated counts from variants with minor allele count [MAC] <= 15',
+  minWidth: 100,
+  accessor: (row) => row.ptv_mis_case_carrier,
+  render: (value) => renderCount(value),
+}
+
+const ptvMisControlCarrierColumn: GeneResultColumnConfig<SchemaGeneRow, number> = {
+  key: 'ptv_mis_control_carrier',
+  heading: 'Control PTV + Missense',
+  tooltip:
+    'Protein-truncating variants (PTVs) classified as high-confidence by LOFTEE: stop-gained, frameshift, and essential splice acceptor and donor sites, and missense variants predicted to be damaging (mean missense rank percentile >= 93%). Aggregated counts from variants with minor allele count [MAC] <= 15',
+  minWidth: 100,
+  accessor: (row) => row.ptv_mis_control_carrier,
+  render: (value) => renderCount(value),
+}
+
+const ptvNDeNovoColumn: GeneResultColumnConfig<SchemaGeneRow, number> = {
+  key: 'ptv_n_de_novo',
+  heading: 'De Novo PTV',
+  tooltip: 'Determined to be de novo of origin in 3,402 parent-proband trios.',
+  minWidth: 90,
+  accessor: (row) => row.ptv_n_de_novo,
+  render: (value) => renderCount(value),
+}
+
+const ptvMisNDeNovoColumn: GeneResultColumnConfig<SchemaGeneRow, number> = {
+  key: 'ptv_mis_n_de_novo',
+  heading: 'De Novo PTV + Missense',
+  tooltip: 'Determined to be de novo of origin in 3,402 parent-proband trios.',
+  minWidth: 100,
+  accessor: (row) => row.ptv_mis_n_de_novo,
+  render: (value) => renderCount(value),
+}
+
+const caseControlPlusDeNovoPValueColumn: GeneResultColumnConfig<SchemaGeneRow, number> = {
+  key: 'case_control_plus_de_novo_p_value',
+  heading: 'Case-Control + de novo P-value',
+  tooltip:
+    'Weighted meta-analysis p-value combining the Case-Control SCHEMA p-value with the De Novo pvalue',
+  minWidth: 100,
+  accessor: (row) => row.case_control_plus_de_novo_p_value,
+  render: (value) => renderStringOrFloatPvalueAsScientific({ value: value }),
+}
+
+const ptvOddsRatioColumn: GeneResultColumnConfig<SchemaGeneRow, string> = {
+  key: 'ptv_odds_ratio',
+  heading: 'OR PTV',
+  tooltip: 'Odds Ratio: The relative increase in schizophrenia risk associated with PTVs.',
+  minWidth: 110,
+  accessor: (row) => row.ptv_odds_ratio,
+  render: (value) => renderOddsRatio({ value: value }),
+}
+
+const ptvMisOddsRatioColumn: GeneResultColumnConfig<SchemaGeneRow, string> = {
+  key: 'ptv_mis_odds_ratio',
+  heading: 'OR PTV + Missense',
+  tooltip:
+    'Odds Ratio: The relative increase in schizophrenia risk associated with PTVs + missense variants predicted to be damaging.',
+  minWidth: 110,
+  accessor: (row) => row.ptv_mis_odds_ratio,
+  render: (value) => renderOddsRatio({ value: value }),
+}
+
+const misCaseCarrierColumn: GeneResultColumnConfig<SchemaGeneRow, number> = {
+  key: 'mis_case_carrier',
+  heading: 'Case Missense',
+  tooltip: 'Missense variants, aggregated counts from variants with minor allele count [MAC] <= 15',
+  minWidth: 100,
+  accessor: (row) => row.mis_case_carrier,
+  render: (value) => renderCount(value),
+}
+
+const misControlCarrierColumn: GeneResultColumnConfig<SchemaGeneRow, number> = {
+  key: 'mis_control_carrier',
+  heading: 'Control Missense',
+  tooltip: 'Missense variants, aggregated counts from variants with minor allele count [MAC] <= 15',
+  minWidth: 100,
+  accessor: (row) => row.mis_control_carrier,
+  render: (value) => renderCount(value),
+}
+
+const synCaseCarrierColumn: GeneResultColumnConfig<SchemaGeneRow, number> = {
+  key: 'syn_case_carrier',
+  heading: 'Case Synonymous',
+  tooltip:
+    'Synonymous variants, aggregated counts from variants with minor allele count [MAC] <= 15',
+  minWidth: 100,
+  accessor: (row) => row.syn_case_carrier,
+  render: (value) => renderCount(value),
+}
+
+const synControlCarrierColumn: GeneResultColumnConfig<SchemaGeneRow, number> = {
+  key: 'syn_control_carrier',
+  heading: 'Control Synonymous',
+  tooltip:
+    'Synonymous variants, aggregated counts from variants with minor allele count [MAC] <= 15',
+  minWidth: 100,
+  accessor: (row) => row.syn_control_carrier,
+  render: (value) => renderCount(value),
+}
+
+const misOddsRatioColumn: GeneResultColumnConfig<SchemaGeneRow, string> = {
+  key: 'mis_odds_ratio',
+  heading: 'OR Missense',
+  tooltip: 'Odds Ratio: The relative increase in schizophrenia risk associated with PTVs.',
+  minWidth: 110,
+  accessor: (row) => row.mis_odds_ratio,
+  render: (value) => renderOddsRatio({ value: value }),
+}
+
+const synOddsRatioColumn: GeneResultColumnConfig<SchemaGeneRow, string> = {
+  key: 'syn_odds_ratio',
+  heading: 'OR Synonymous',
+  tooltip: 'Odds Ratio: The relative increase in schizophrenia risk associated with PTVs.',
+  minWidth: 110,
+  accessor: (row) => row.syn_odds_ratio,
+  render: (value) => renderOddsRatio({ value: value }),
+}
+
+const schemaGeneResultColumns: GeneResultColumnConfig<SchemaGeneRow, any>[] = [
+  schemaCaseControlPValueColumn,
+  ptvCaseCarrierColumn,
+  ptvControlCarrierColumn,
+  ptvMisCaseCarrierColumn,
+  ptvMisControlCarrierColumn,
+  ptvNDeNovoColumn,
+  ptvMisNDeNovoColumn,
+  caseControlPlusDeNovoPValueColumn,
+  ptvOddsRatioColumn,
+  ptvMisOddsRatioColumn,
+  misCaseCarrierColumn,
+  misControlCarrierColumn,
+  synCaseCarrierColumn,
+  synControlCarrierColumn,
+  misOddsRatioColumn,
+  synOddsRatioColumn,
+]
+
 const schemaVariantNDeNovoColumn: VariantColumnConfig<SchemaVariantRow, number> = {
   key: 'group_result.n_de_novo',
   heading: 'No. de novos',
@@ -124,12 +298,12 @@ const renderSchemaVariantAttributes: RenderVariantAttributes<SchemaVariantInfo> 
   misfit_s: misfitS,
   pop_eve: popEve,
 }) => [
-  { label: 'MisRank Percentile', content: misrankPercentile === null ? '–' : misrankPercentile },
-  { label: 'MPC', content: mpc === null ? '–' : mpc },
-  { label: 'AlphaMissense', content: alphaMissense === null ? '–' : alphaMissense },
-  { label: 'MisFit S', content: misfitS === null ? '–' : misfitS },
-  { label: 'PopEVE', content: popEve === null ? '–' : popEve },
-]
+    { label: 'MisRank Percentile', content: misrankPercentile === null ? '–' : misrankPercentile },
+    { label: 'MPC', content: mpc === null ? '–' : mpc },
+    { label: 'AlphaMissense', content: alphaMissense === null ? '–' : alphaMissense },
+    { label: 'MisFit S', content: misfitS === null ? '–' : misfitS },
+    { label: 'PopEVE', content: popEve === null ? '–' : popEve },
+  ]
 
 const SCHEMABrowser = () => (
   <ExomeResultsBrowser
@@ -152,131 +326,7 @@ const SCHEMABrowser = () => (
     geneResultAnalysisGroupOptions={schemaAnalysisGroups}
     defaultGeneResultAnalysisGroup={schemaDefaultAnalysisGroup}
     defaultGeneResultSortKey="schema_case_control_p_value"
-    geneResultColumns={[
-      {
-        key: 'schema_case_control_p_value',
-        heading: 'Case-Control SCHEMA P-value',
-        tooltip:
-          'SCHEMA p-value of the CMH p-value from PTV burden and the CMH p-value from PTV + missense burden.',
-        minWidth: 100,
-        render: (value) => renderStringOrFloatPvalueAsScientific({ value: value }),
-      },
-      {
-        key: 'ptv_case_carrier',
-        heading: 'Case PTV',
-        tooltip:
-          'Protein-truncating variants (PTVs) classified as high-confidence by LOFTEE: stop-gained, frameshift, and essential splice acceptor and donor sites. Aggregated counts from variants with minor allele count [MAC] <= 15',
-        minWidth: 70,
-        render: (value) => renderCount(value),
-      },
-      {
-        key: 'ptv_control_carrier',
-        heading: 'Control PTV',
-        tooltip:
-          'Protein-truncating variants (PTVs) classified as high-confidence by LOFTEE: stop-gained, frameshift, and essential splice acceptor and donor sites. Aggregated counts from variants with minor allele count [MAC] <= 15',
-        minWidth: 70,
-        render: (value) => renderCount(value),
-      },
-      {
-        key: 'ptv_mis_case_carrier',
-        heading: 'Case PTV + Missense',
-        tooltip:
-          'Protein-truncating variants (PTVs) classified as high-confidence by LOFTEE: stop-gained, frameshift, and essential splice acceptor and donor sites, and missense variants predicted to be damaging (mean missense rank percentile >= 93%). Aggregated counts from variants with minor allele count [MAC] <= 15',
-        minWidth: 100,
-        render: (value) => renderCount(value),
-      },
-      {
-        key: 'ptv_mis_control_carrier',
-        heading: 'Control PTV + Missense',
-        tooltip:
-          'Protein-truncating variants (PTVs) classified as high-confidence by LOFTEE: stop-gained, frameshift, and essential splice acceptor and donor sites, and missense variants predicted to be damaging (mean missense rank percentile >= 93%). Aggregated counts from variants with minor allele count [MAC] <= 15',
-        minWidth: 100,
-        render: (value) => renderCount(value),
-      },
-      {
-        key: 'ptv_n_de_novo',
-        heading: 'De Novo PTV',
-        tooltip: 'Determined to be de novo of origin in 3,402 parent-proband trios.',
-        minWidth: 90,
-        render: (value) => renderCount(value),
-      },
-      {
-        key: 'ptv_mis_n_de_novo',
-        heading: 'De Novo PTV + Missense',
-        tooltip: 'Determined to be de novo of origin in 3,402 parent-proband trios.',
-        minWidth: 100,
-        render: (value) => renderCount(value),
-      },
-      {
-        key: 'case_control_plus_de_novo_p_value',
-        heading: 'Case-Control + de novo P-value',
-        tooltip:
-          'Weighted meta-analysis p-value combining the Case-Control SCHEMA p-value with the De Novo pvalue',
-        minWidth: 100,
-        render: (value) => renderStringOrFloatPvalueAsScientific({ value: value }),
-      },
-      {
-        key: 'ptv_odds_ratio',
-        heading: 'OR PTV',
-        tooltip: 'Odds Ratio: The relative increase in schizophrenia risk associated with PTVs.',
-        minWidth: 110,
-        render: (value) => renderOddsRatio({ value: value }),
-      },
-      {
-        key: 'ptv_mis_odds_ratio',
-        heading: 'OR PTV + Missense',
-        tooltip:
-          'Odds Ratio: The relative increase in schizophrenia risk associated with PTVs + missense variants predicted to be damaging.',
-        minWidth: 110,
-        render: (value) => renderOddsRatio({ value: value }),
-      },
-      {
-        key: 'mis_case_carrier',
-        heading: 'Case Missense',
-        tooltip:
-          'Missense variants, aggregated counts from variants with minor allele count [MAC] <= 15',
-        minWidth: 100,
-        render: (value) => renderCount(value),
-      },
-      {
-        key: 'mis_control_carrier',
-        heading: 'Control Missense',
-        tooltip:
-          'Missense variants, aggregated counts from variants with minor allele count [MAC] <= 15',
-        minWidth: 100,
-        render: (value) => renderCount(value),
-      },
-      {
-        key: 'syn_case_carrier',
-        heading: 'Case Synonymous',
-        tooltip:
-          'Synonymous variants, aggregated counts from variants with minor allele count [MAC] <= 15',
-        minWidth: 100,
-        render: (value) => renderCount(value),
-      },
-      {
-        key: 'syn_control_carrier',
-        heading: 'Control Synonymous',
-        tooltip:
-          'Synonymous variants, aggregated counts from variants with minor allele count [MAC] <= 15',
-        minWidth: 100,
-        render: (value) => renderCount(value),
-      },
-      {
-        key: 'mis_odds_ratio',
-        heading: 'OR Missense',
-        tooltip: 'Odds Ratio: The relative increase in schizophrenia risk associated with PTVs.',
-        minWidth: 110,
-        render: (value) => renderOddsRatio({ value: value }),
-      },
-      {
-        key: 'syn_odds_ratio',
-        heading: 'OR Synonymous',
-        tooltip: 'Odds Ratio: The relative increase in schizophrenia risk associated with PTVs.',
-        minWidth: 110,
-        render: (value) => renderOddsRatio({ value: value }),
-      },
-    ]}
+    geneResultColumns={schemaGeneResultColumns}
     geneResultTabs={[
       {
         id: 'manhattan-plot',
