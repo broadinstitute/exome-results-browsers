38c730691ca57c2966140971a404c0e6c601cfb2
Riley Grant <grantrileyh@gmail.com>
2026-09-13 12:56:41 -0500

    test(frontend): add SCHEMA variant modal test

--- jest.config.js
+++ jest.config.js
@@ -5,6 +5,7 @@ module.exports = {
   testMatch: ['<rootDir>/src/**/*.test.{ts,tsx}'],
   moduleNameMapper: {
     '\\.md$': '<rootDir>/src/browsers/testUtils/markdownStub.ts',
+    '\\.(gif|jpg|png|svg)$': '<rootDir>/src/browsers/testUtils/fileStub.ts',
   },
   transform: {
     '^.+\\.tsx?$': ['ts-jest', { isolatedModules: true }],
