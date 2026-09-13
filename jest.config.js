96fdea9c0fcf93c9b35e3e304a404e48c4a1acc7
Riley Grant <grantrileyh@gmail.com>
2026-09-13 12:55:32 -0500

    test(frontend): add SCHEMA single gene result table test

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,9 @@ module.exports = {
   testEnvironment: 'jsdom',
   setupFilesAfterEnv: ['<rootDir>/src/browsers/jest.setup.ts'],
   testMatch: ['<rootDir>/src/**/*.test.{ts,tsx}'],
+  moduleNameMapper: {
+    '\\.md$': '<rootDir>/src/browsers/testUtils/markdownStub.ts',
+  },
   transform: {
     '^.+\\.tsx?$': ['ts-jest', { isolatedModules: true }],
   },
